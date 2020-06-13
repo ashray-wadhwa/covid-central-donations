104b1a12be330c0cee10e2e4a0bfd62c7455acfc
Ashray Wadhwa <ashraywadhwa@Ashrays-MBP.Dlink>
2020-06-14 03:46:24 +0530

    Updates

---
+++ precache-manifest.6e055926bf9102de81142d1a90941bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc52b0569acd4ef12f0c46e8dd95e6e",
+    "url": "/covid-central-donations/index.html"
+  },
+  {
+    "revision": "1474b5d6f3c5e55f3624",
+    "url": "/covid-central-donations/static/css/main.4026271d.chunk.css"
+  },
+  {
+    "revision": "3c67a2005a06e814fc3b",
+    "url": "/covid-central-donations/static/js/2.8dff3abf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-central-donations/static/js/2.8dff3abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1474b5d6f3c5e55f3624",
+    "url": "/covid-central-donations/static/js/main.70ae0dcd.chunk.js"
+  },
+  {
+    "revision": "fd420e04e9b49eb58fa6",
+    "url": "/covid-central-donations/static/js/runtime-main.58a87c6b.js"
+  }
+]);
