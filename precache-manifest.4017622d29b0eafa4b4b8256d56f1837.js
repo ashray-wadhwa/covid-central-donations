2a78b53e79984a2c0ae57494470545b71f4cb573
Ashray Wadhwa <ashraywadhwa@ashrays-mbp.umd.edu>
2021-06-08 12:00:56 -0400

    Updates

--- precache-manifest.4017622d29b0eafa4b4b8256d56f1837.js
+++ precache-manifest.4017622d29b0eafa4b4b8256d56f1837.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0fd2caf12f1677f15becd1fac95141e3",
+    "revision": "7f52bd5847e77c0c98770594af2651d9",
     "url": "/covid-central-donations/index.html"
   },
   {
-    "revision": "6d1516a5a978353dad02",
-    "url": "/covid-central-donations/static/css/main.8f6c244f.chunk.css"
+    "revision": "60de8a4b1fd8a792aa0e",
+    "url": "/covid-central-donations/static/css/main.c71ccc4d.chunk.css"
   },
   {
     "revision": "3c67a2005a06e814fc3b",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid-central-donations/static/js/2.8dff3abf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6d1516a5a978353dad02",
+    "revision": "60de8a4b1fd8a792aa0e",
     "url": "/covid-central-donations/static/js/main.45021048.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-central-donations/precache-manifest.87357a0d0756dc96b70c257451980327.js"
+  "/covid-central-donations/precache-manifest.4017622d29b0eafa4b4b8256d56f1837.js"
 );
 
 self.addEventListener('message', (event) => {
