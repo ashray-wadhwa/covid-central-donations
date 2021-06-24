ca4538d1e89518b9ad6f7170ada93a6e0ec8e806
Ashray Wadhwa <ashraywadhwa@wireless-10-104-229-247.umd.edu>
2021-06-23 22:31:09 -0400

    Updates

--- precache-manifest.e375013bd449fd8689f7ca85baadb5c0.js
+++ precache-manifest.e375013bd449fd8689f7ca85baadb5c0.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9bcbee89f6fa2cac2e38730b691275e2",
+    "revision": "81a9244e4bcb7c587ca0a6a6c28c7bb1",
     "url": "/covid-central-donations/index.html"
   },
   {
-    "revision": "d1a68278f45306649e63",
-    "url": "/covid-central-donations/static/css/main.a355819c.chunk.css"
+    "revision": "ab013b7124f8e39af56e",
+    "url": "/covid-central-donations/static/css/main.ab6a5ea1.chunk.css"
   },
   {
     "revision": "3c67a2005a06e814fc3b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid-central-donations/static/js/2.8dff3abf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d1a68278f45306649e63",
-    "url": "/covid-central-donations/static/js/main.30024c6b.chunk.js"
+    "revision": "ab013b7124f8e39af56e",
+    "url": "/covid-central-donations/static/js/main.778796cd.chunk.js"
   },
   {
     "revision": "fd420e04e9b49eb58fa6",
